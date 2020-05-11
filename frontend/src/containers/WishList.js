b944049cee5d08cef5604052fda29e76d5c064fe
KGoki <kamil.golaszewski.ncf@gmail.com>
2020-05-11 17:22:36 -0400

    Add Delete button on Wishlist page

--- frontend/src/containers/WishList.js
+++ frontend/src/containers/WishList.js
@@ -5,7 +5,7 @@ import CustomModal from  '../components/Modal';
 import axios from 'axios';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { connect } from 'react-redux';
 import * as actions from '../store/actions/auth';
@@ -44,20 +44,26 @@ class App extends Component {
                 ...this.state.activeItem, 
                 id: active_id,
                 game: active_game,
-        }
+            }
         }) 
         console.log(this.state)
     }
 
-    handleEdit = (item) => {
-        item = {
-          ...item,
-        //   target_price: parseFloat(item.target_price)
-        }
+    handleDelete = (item_id) => {
         axios
-          .put(`http://localhost:8001/api/wishlistitems/${item.id}/`, item)
-          .then(res => { console.log(item) })
-          .catch(err => { console.log('State of item during error'); console.log(item); console.log(err);})
+            .delete(`http://localhost:8001/api/wishlistitems/${item_id}`)
+            .then(res => { console.log(res) })
+            .catch(err => { console.log('State of item during error'); console.log(err);})
+        let updated_wishlist = this.state.wishlistitems.filter(e => e.id !== item_id)
+        this.setState({ wishlistitems: updated_wishlist })
+    }
+
+    handleEdit = (item) => {
+        // item = { ...item, target_price: parseFloat(item.target_price) }
+        axios
+            .put(`http://localhost:8001/api/wishlistitems/${item.id}/`, item)
+            .then(res => { console.log(item) })
+            .catch(err => { console.log('State of item during error'); console.log(item); console.log(err);})
         let updated_wishlist = this.state.wishlistitems.map(e => {
             if (e.id === item.id) {
                 return {
@@ -80,7 +86,7 @@ class App extends Component {
             <h1>My Wishlist</h1>
             <div className="container">
             <div className="row">
-                <div className="offset-2 col-md-8">
+                <div className="offset-1 col-md-10">
                 { this.state.wishlistitems.map(item => {
                     let { game } = item
                     return (
@@ -89,6 +95,10 @@ class App extends Component {
                         <button className="btn btn-success pad-r" id={game.id} 
                             onClick={() => this.toggle(item.id, game.id)}>
                             <FontAwesomeIcon icon={faEdit} size='1x' style={{color: '#fff'}}/>
+                        </button>
+                        <button className="btn btn-danger pad-r" id={game.id} 
+                            onClick={() => this.handleDelete(item.id)}>
+                            <FontAwesomeIcon icon={faTrashAlt} size='1x' style={{color: '#fff'}}/>
                         </button>
                         <img className="offset-0" height="55" alt=""
                         src={`https://steamcdn-a.akamaihd.net/steam/apps/${game.steam_id}/capsule_184x69.jpg`} 

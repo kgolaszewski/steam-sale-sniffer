50426f781ff66bce30c58c781b82df4334fd7ce2
KGoki <kamil.golaszewski.ncf@gmail.com>
2020-05-10 22:32:53 -0400

    Add Edit button for target_price

--- frontend/src/containers/WishList.js
+++ frontend/src/containers/WishList.js
@@ -1,11 +1,14 @@
+// import { withRouter } from 'react-router-dom'
 import React, { Component } from 'react';
 import '../App.css';
 import CustomModal from  '../components/Modal';
 import axios from 'axios';
 
-// import { withRouter } from 'react-router-dom'
-import { connect } from 'react-redux'
-import * as actions from '../store/actions/auth'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+
+import { connect } from 'react-redux';
+import * as actions from '../store/actions/auth';
 
 class App extends Component {
     constructor(props) {
@@ -17,6 +20,7 @@ class App extends Component {
             game: '',
             user: localStorage.getItem('userId'),
             target_price: "",
+            id: '',
         }
         }
     }
@@ -31,16 +35,43 @@ class App extends Component {
             .catch( err => console.log(err) )
     }
 
-    toggle = (app_id) => { 
-        let new_id = !this.state.modal ? app_id : ''
+    toggle = (id, game) => { 
+        let active_id = !this.state.modal ? id : ''
+        let active_game = !this.state.modal ? game : ''
         this.setState({ 
             modal: !this.state.modal, 
             activeItem: {
                 ...this.state.activeItem, 
-                game: new_id
+                id: active_id,
+                game: active_game,
         }
         }) 
         console.log(this.state)
+    }
+
+    handleEdit = (item) => {
+        item = {
+          ...item,
+        //   target_price: parseFloat(item.target_price)
+        }
+        axios
+          .put(`http://localhost:8001/api/wishlistitems/${item.id}/`, item)
+          .then(res => { console.log(item) })
+          .catch(err => { console.log('State of item during error'); console.log(item); console.log(err);})
+        let updated_wishlist = this.state.wishlistitems.map(e => {
+            if (e.id === item.id) {
+                return {
+                    ...e, 
+                    target_price: item.target_price
+                }
+            } else { 
+                return e
+            }
+        })
+        this.setState({ 
+          wishlistitems: updated_wishlist
+        })
+        this.toggle();
     }
 
     render() {
@@ -53,22 +84,29 @@ class App extends Component {
                 { this.state.wishlistitems.map(item => {
                     let { game } = item
                     return (
+                    <div>
                     <div className="row game" key={game.id}>
+                        <button className="btn btn-success pad-r" id={game.id} 
+                            onClick={() => this.toggle(item.id, game.id)}>
+                            <FontAwesomeIcon icon={faEdit} size='1x' style={{color: '#fff'}}/>
+                        </button>
                         <img className="offset-0" height="55" alt=""
                         src={`https://steamcdn-a.akamaihd.net/steam/apps/${game.steam_id}/capsule_184x69.jpg`} 
                         />
-                        <div className="col-md-5 game-title text">{game.title}</div>
+                        <div className="col-md-4 game-title text">{game.title}</div>
                         <div className="col-md-2 game-price text">${item.target_price}</div>
                         <div className="col-md-2 game-price text">${game.curr_price}</div>
+                        {/* <div>{item.id}</div> */}
+                    </div>
                     </div>
                     )
                 })}
                 </div>
             </div>
             </div>
-            {/* { this.state.modal ? (
-            <CustomModal activeItem={this.state.activeItem} toggle={this.toggle} onSave={this.handleSubmit} />
-            ) : null } */}
+            { this.state.modal ? (
+            <CustomModal activeItem={this.state.activeItem} toggle={this.toggle} onSave={this.handleEdit} />
+            ) : null }
         </div>
         );
     }

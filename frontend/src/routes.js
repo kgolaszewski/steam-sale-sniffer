44ce61c06412cb63fe5781617764bc14133faf42
KGoki <kamil.golaszewski.ncf@gmail.com>
2020-05-18 11:05:15 -0400

    Add <PrivateRoute /> (per: JustDjango facial-recognition-sass)

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Hoc from './hoc'
 
 import GameList from './containers/GameList'
@@ -8,12 +8,28 @@ import Signup from './containers/Signup'
 import WishList from './containers/WishList'
 import Collection from './containers/Collection'
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const authenticated = localStorage.getItem("userId") !== null;
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          authenticated === true ? (
+            <Component {...props} />
+          ) : ( <Redirect to={{ pathname: "/login", state: { from: props.location } }} /> )
+        }
+      />
+    );
+  };
+
 const BaseRouter = () => (
     <Hoc>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
-        <Route exact path="/wishlist" component={WishList} />
-        <Route exact path="/collection" component={Collection} />
+        {/* <Route exact path="/wishlist" component={WishList} /> */}
+        {/* <Route exact path="/collection" component={Collection} /> */}
+        <PrivateRoute path="/wishlist" component={WishList} />
+        <PrivateRoute path="/collection" component={Collection} />
         <Route exact path="/" component={GameList} />
     </Hoc>
 )
